507b9f57592de6f6ef35e37058ef3f96f6d49e5e
Nathan Monteiro <contato.mont@gmail.com>
2022-11-15 16:22:36 -0300

    Big update!
    Finally, figure it out the issue with the sun and moon images and yes, the light/dark mode is going to happend. Changing some images to see if it's possible

--- script.js
+++ script.js
@@ -3,14 +3,13 @@
 /*VARIABLES*/
 const openMenu = document.querySelector('.hamb-menu');
 const optionsMenu = document.querySelectorAll('.options');
+const logoCompany = document.getElementById('logo-company');
 
 /*DARK/LIGHT MODE VARIABLES*/
 const darkMode = document.getElementById('moon');
 const lightMode = document.getElementById('sun');
-// const darkMode = document.getElementById('dark-mode');
-// const lightMode = document.getElementById('light-mode');
-// const body = document.querySelector('body');
-// const optionLinks = document.querySelectorAll('.menu-links')
+const body = document.querySelector('body');
+const links = document.querySelectorAll('.links')
 
 /* OPENING THE HAMBURGER MENU OPTIONS */
 for (let i=0 ; i<optionsMenu.length ; i++) {
@@ -21,8 +20,34 @@ for (let i=0 ; i<optionsMenu.length ; i++) {
 
 //SWITCH TO DARK MODE
 darkMode.addEventListener('click', function () {
-  console.log('oola');
+  body.style.backgroundColor = "black";
+  darkMode.src = `/cozy/assets/images/icons/moon-mode-${2}.png`;
+  lightMode.src = `/cozy/assets/images/icons/sun-mode-${2}.png`;
+  logoCompany.src = `/cozy/assets/images/icons/staircase-logo-${2}.png`;
+  openMenu.src = `/cozy/assets/images/icons/menu-hamb-${2}.png`;
+  
+  console.log('dark mode');
+  
+  for (let i=0 ; i<links.length ; i++) {
+    links[i].style.color = "white";
+  }
 });
+
+//SWITCH TO LIGHT MODE
+lightMode.addEventListener('click', function () {
+  body.style.backgroundColor = "white";
+  darkMode.src = `/cozy/assets/images/icons/moon-mode-1.png`;
+  lightMode.src = `/cozy/assets/images/icons/sun-mode-1.png`;
+  logoCompany.src = `/cozy/assets/images/icons/staircase-logo-1.png`;
+  openMenu.src = `/cozy/assets/images/icons/menu-hamb-1.png`;
+  
+  console.log('light mode');
+  
+  for (let i=0 ; i<links.length ; i++) {
+    links[i].style.color = "black";
+  }
+});
+
 // darkMode.addEventListener('click', function () {
   
   /* STYLING THE MODES */
@@ -39,11 +64,7 @@ darkMode.addEventListener('click', function () {
 //   }
 // });
 
-//SWITCH TO LIGHT MODE
-lightMode.addEventListener('click', function () {
-  let text = "hello tds!!!"
-  console.log(text);
-});
+
 // lightMode.addEventListener('click', function () {
   
   /* STYLING THE MODES */
@@ -60,11 +81,3 @@ lightMode.addEventListener('click', function () {
 //     lightMode.src = `/cozy/assets/images/icons/moon-mode-1.png`;
 //   }
 // });
-
-/* DARK MODE */
-// for (let i=0 ; i<optionsMenu.length ; i++) {
-//   console.log('teste');
-//   btnOpenCloseMenuWhite.addEventListener('click', function () {
-//     optionsMenu[i].classList.toggle('hidden');
-//   });
-// }
