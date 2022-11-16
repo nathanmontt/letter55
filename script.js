34c025119e6db9ea7ec24d7dc637bbd5eba0e07c
Nathan Monteiro <contato.mont@gmail.com>
2022-11-15 23:53:11 -0300

    Updating p.5
    Trying to solve the background issue while my copmuter's lagging. Delete of some old code on the script.js file

--- script.js
+++ script.js
@@ -26,8 +26,6 @@ darkMode.addEventListener('click', function () {
   logoCompany.src = `/cozy/assets/images/icons/staircase-logo-${2}.png`;
   openMenu.src = `/cozy/assets/images/icons/menu-hamb-${2}.png`;
   
-  console.log('dark mode');
-  
   for (let i=0 ; i<links.length ; i++) {
     links[i].style.color = "white";
   }
@@ -41,43 +39,7 @@ lightMode.addEventListener('click', function () {
   logoCompany.src = `/cozy/assets/images/icons/staircase-logo-1.png`;
   openMenu.src = `/cozy/assets/images/icons/menu-hamb-1.png`;
   
-  console.log('light mode');
-  
   for (let i=0 ; i<links.length ; i++) {
     links[i].style.color = "black";
   }
 });
-
-// darkMode.addEventListener('click', function () {
-  
-  /* STYLING THE MODES */
-  // darkMode.style.width = "2rem";
-  // darkMode.style.height = "2rem";
-  // darkMode.style.marginRight = "0.2rem";
-  
-//   for (let i=0 ; i<optionLinks.length ; i++) {
-//     optionLinks[i].style.color = "white";
-//     body.style.backgroundColor = "black";
-
-//     darkMode.src = `/cozy/assets/images/icons/sun-mode-${2}.png`;
-//     lightMode.src = `/cozy/assets/images/icons/moon-mode-${2}.png`;
-//   }
-// });
-
-
-// lightMode.addEventListener('click', function () {
-  
-  /* STYLING THE MODES */
-  // lightMode.style.width = "3rem";
-  // lightMode.style.height = "3rem";
-  // lightMode.style.marginRight = "0.5rem;";
-  
-  
-//   for (let i=0 ; i<optionLinks.length ; i++) {
-//     optionLinks[i].style.color = "black";
-
-//     body.style.backgroundColor = "white";
-//     darkMode.src = `/cozy/assets/images/icons/sun-mode-1.png`;
-//     lightMode.src = `/cozy/assets/images/icons/moon-mode-1.png`;
-//   }
-// });
