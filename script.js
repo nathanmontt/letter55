ed0d2632ef6c136a3d10360d2cdb4cc9398974e5
Nathan Monteiro <contato.mont@gmail.com>
2022-11-22 17:08:00 -0300

    New updates! (p.1)
    Changing the name of the project and the concept behind it

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
 'use strict'
 
 /*VARIABLES*/
-const openMenu = document.querySelector('.hamb-menu');
-const optionsMenu = document.querySelectorAll('.options');
-const logoCompany = document.getElementById('logo-company');
