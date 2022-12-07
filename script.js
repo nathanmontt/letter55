09096bea9e5e2623321e560c6519b1ca10c0ee69
Nathan Monteiro <contato.mont@gmail.com>
2022-12-07 01:37:03 -0300

    Tests
    Found some error on the JavaScript file. Still trying to figure out what it is

--- script.js
+++ script.js
@@ -1,14 +1,28 @@
 'use strict'
 
 /* VARIABLES */
-const transformToWhiteMode = document.getElementById("sun");
-const transformToDarkMode = document.getElementById("moon");
+const transformToWhiteMode = document.querySelector("#sun");
+const transformToDarkMode = document.querySelector("#moon");
 
 /* VARIABLES THAT ARE GOIN TO BE AFFECTED BY THE SWITCHING OF DARK/LIGH MODE */
-const Options = document.querySelectorAll(".options");
-const companySomeText = document.querySelector(".title-company");
-const companySomeDescriptionText = document.querySelector(".description");
+const menuOptions = document.querySelectorAll(".options");
+// const companySomeText = document.querySelector(".title-company");
+// const companySomeDescriptionText = document.querySelector(".description");
 
 transformToDarkMode.addEventListener("click", function () {
-  
+  for (let i = 0; i < menuOptions.length; i++) {
+
+    // transformToDarkMode.src = `/Furnace/assets/images/icons/sun-mode-${1}.png`;
+    // transformToDarkMode.src = `/Furnace/assets/images/icons/moon-mode-${1}.png`;
+  }
 });
+console.log("teste");
+
+for (let i = 0; i < menuOptions.length; i++) {
+  console.log("ola");
+  transformToDarkMode.addEventListener("click", function () {
+    console.log("ola3");
+    menuOptions[i].style.color = "black";
+    menuOptions[i].style.border = "thick solid #0000FF";
+  });
+}
