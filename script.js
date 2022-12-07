17db7a09d3b004cc7cb5e43214739c299228b8c2
Nathan Monteiro <contato.mont@gmail.com>
2022-12-07 01:14:09 -0300

    Update script.js
    Initiating the process of light/dark mode

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 'use strict'
+
+/* VARIABLES */
+const transformToWhiteMode = document.getElementById("sun");
+const transformToDarkMode = document.getElementById("moon");
+
+/* VARIABLES THAT ARE GOIN TO BE AFFECTED BY THE SWITCHING OF DARK/LIGH MODE */
+const Options = document.querySelectorAll(".options");
+const companySomeText = document.querySelector(".title-company");
+const companySomeDescriptionText = document.querySelector(".description");
+
+transformToDarkMode.addEventListener("click", function () {
+  
+});
