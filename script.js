df3cd458548af90d816e34abb87e135ecc532e50
Nathan Monteiro <contato.mont@gmail.com>
2022-12-26 16:57:02 -0300

    Update script.js
    Adding some re-usable code for the application

---
+++ script.js
@@ -0,0 +1,10 @@
+'use strict'
+
+//RESIZE ON WINDOWN
+// window.addEventListener('resize', function () {
+//   if (window.innerWidth >= 700) {
+//     teste.style.background = "#62ff01"
+//   } else {
+//     teste.style.background = "black"
+//   }
+// });
